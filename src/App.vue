052e45c98989249b63a1492f9804274fffc72cd0
Hermann EHO <hermannleboss@gmail.com>
2023-04-02 12:37:29 +0000

    feat: style token and style input

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {onMounted, ref} from "vue";
 import type {Ref} from 'vue'
+import {ethers} from "ethers";
 import {Alchemy, Network, Utils} from 'alchemy-sdk';
 
 const userAddress = ref("")
@@ -9,6 +10,9 @@ const tokenDataObjects: Ref<Array<any>> = ref([])
 let tokens: Ref = ref()
 const isLoading = ref(false)
 
+const isAddress = () => {
+    return ethers.isAddress(userAddress.value)
+}
 const isWalletConnected = async () => {
     let status = false
     try {
@@ -65,7 +69,7 @@ async function checkToken() {
 
     const alchemy = new Alchemy(config);
     tokens.value = await alchemy.core.getTokenBalances(userAddress.value)
-    console.log("my tokens",tokens.value)
+    console.log("my tokens", tokens.value)
 
     const tokenDataPromises = [];
 
@@ -97,26 +101,30 @@ async function checkToken() {
                 :value="userAddress"
                 @input="event => userAddress = event.target.value"
                 placeholder="Type And address"
-                class="mt-1 inline-block w-[25%] px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-    ">
-        <button class="bg-blue-600 text-white rounded-lg border p-3 m-4" @click="checkToken()">Check ERC-20 Token
+                class="mt-1 inline-block w-[25%] px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400"
+                :class="{
+                    'focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500': isAddress(),
+                    'focus:outline-none focus:ring-1  focus:border-red-500 focus:ring-red-500 text-red-600': !isAddress()
+                }"
+        >
+        <p v-if="!isAddress()"> Type a valid address</p>
+        <button class="bg-blue-600 text-white rounded-lg border p-3 m-4" @click="checkToken()" :disabled="!isAddress()">Check ERC-20 Token
             Balances
         </button>
 
         <h2 class="text-xl font-bold py-3">ERC-20 token balances:</h2>
         <div class="flex gap-5 flex-wrap" v-if="!isLoading && tokenIsLoaded">
 
-            <div class="border bg-white w-[20vw] min-h-[250px]" v-for="(token, index) in tokens.tokenBalances" :key="index">
-                <p>Symbol: $ {{tokenDataObjects[index].symbol}}&nbsp;</p>
-                <p>Balance: {{Utils.formatUnits(
-                    token.tokenBalance,
-                    tokenDataObjects[index].decimals
-                )}}</p>
-                <img :src="tokenDataObjects[index].logo" :alt="tokenDataObjects[index].symbol" class="w-100">
+            <div class="border bg-white w-[20vw] min-h-[250px]" v-for="(token, index) in tokens.tokenBalances"
+                 :key="index">
+                <p>Symbol: $ {{ tokenDataObjects[index].symbol }}&nbsp;</p>
+                <p>Balance: {{
+                    Utils.formatUnits(
+                        token.tokenBalance,
+                        tokenDataObjects[index].decimals
+                    )
+                    }}</p>
+                <img :src="tokenDataObjects[index].logo ? tokenDataObjects[index].logo : 'https://placehold.co/400?text=No+image+found' " :alt="tokenDataObjects[index].symbol" style="width: 100%">
             </div>
         </div>
     </main>
